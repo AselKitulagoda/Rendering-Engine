66ab0d3328303dbd695576254e9f99c819a6df3b
asel <asel.kitulagoda@gmail.com>
2020-02-26 18:03:41 +0000

    added simons

--- Lab3/0D-RedNoise/RedNoise.cpp
+++ Lab3/0D-RedNoise/RedNoise.cpp
@@ -13,16 +13,12 @@ using namespace glm;
 
 #define WIDTH 500
 #define HEIGHT 500
-#define SCALE_FACTOR 40
+#define SCALE_FACTOR 0.3
 #define PI 3.1415
-// #define MTLPATH "/home/asel/Documents/ComputerGraphics/Lab3/cornell-box.mtl"
-// #define OBJPATH "/home/asel/Documents/ComputerGraphics/Lab3/cornell-box.obj"
-#define MTLPATH "/home/ks17226/Documents/ComputerGraphics/Lab3/cornell-box.mtl"
-#define OBJPATH "/home/ks17226/Documents/ComputerGraphics/Lab3/cornell-box.obj"
-// #define MTLPATH "/home/ak17520/Documents/ComputerGraphics/Lab3/cornell-box.mtl"
-// #define OBJPATH "/home/ak17520/Documents/ComputerGraphics/Lab3/cornell-box.obj"
+#define MTLPATH "/home/asel/Documents/ComputerGraphics/Lab3/cornell-box.mtl"
+#define OBJPATH "/home/asel/Documents/ComputerGraphics/Lab3/cornell-box.obj"
 
-vec3 cameraPos(0, 0, 300);
+vec3 cameraPos(0, 0, 6);
 mat3 cameraOrientation = mat3(vec3(1, 0, 0),
                               vec3(0, 1, 0),
                               vec3(0, 0, 1));
@@ -73,8 +69,9 @@ int main(int argc, char* argv[])
     if(window.pollForInputEvents(&event))
     {
       handleEvent(event);
+      // drawFilledWireframe(triangl);
       depthBuffer(triangl);
-    } 
+    }
     update();
 
     // Need to render the frame at the end, or nothing actually gets shown on the screen !
@@ -318,38 +315,35 @@ vector<ModelTriangle> readObj(float scale)
 return tris;
 }
 
-CanvasTriangle modelToCanvas(ModelTriangle t)
+CanvasTriangle modelToCanvas(ModelTriangle modelTrig)
 {
-  float f=250; /* camera distance, some negative val */
-  CanvasTriangle triangle;
-  triangle.colour = t.colour;
-  for(int i = 0; i < 3; i++)
-  {
-    vec3 pWorld = t.vertices[i];
-    float xCamera = pWorld.x - cameraPos.x;
-    float yCamera = pWorld.y - cameraPos.y;
-    float zCamera = pWorld.z - cameraPos.z;
-
-    vec3 cameraToVertex = vec3(xCamera, yCamera, zCamera);
-    // vec3 cameraToVertex = pWorld;
-    vec3 adjustedVector = cameraToVertex * cameraOrientation;
-
-    float pScreen = f/-zCamera;
-    int xProj = std::floor(adjustedVector.x*pScreen) + WIDTH/2;
-    int yProj = std::floor((-adjustedVector.y)*pScreen) + HEIGHT/2;
-
-    CanvasPoint p = CanvasPoint(xProj, yProj);
-    p.depth = 1/-adjustedVector.z;
-    triangle.vertices[i] = p;
-  }
-  return triangle;
+    float f = 3;
+    CanvasTriangle canvasTrig = CanvasTriangle();
+    canvasTrig.colour = modelTrig.colour;
+    for(int i=0; i<3 ;i++) {
+        float xdistance = modelTrig.vertices[i].x-cameraPos.x;
+        float ydistance = modelTrig.vertices[i].y-cameraPos.y;
+        float zdistance = modelTrig.vertices[i].z-cameraPos.z;
+        vec3 cameraToVertex = vec3(xdistance, ydistance, zdistance);
+        vec3 adjustedVector = cameraToVertex * cameraOrientation;
+        float pScreen = f/-adjustedVector.z;
+        // Scale up the x and y canvas coords to get a bigger image (rather than a big model loader scaling)
+        float canvasScaling = 150;
+        float xProj = (adjustedVector.x*pScreen*canvasScaling) + WIDTH/2;
+        float yProj = (-adjustedVector.y*pScreen*canvasScaling) + HEIGHT/2;
+        CanvasPoint p = CanvasPoint(xProj, yProj);
+        p.depth = 1.0/-adjustedVector.z;
+        canvasTrig.vertices[i] = p;
+    }
+    return canvasTrig;
 }
 
 void drawFilledWireframe(vector <ModelTriangle> tris){
+  window.clearPixels();
   for (size_t i=0;i<tris.size();i++){
     // cout << i << endl;
     CanvasTriangle new_tri = modelToCanvas(tris[i]);
-    drawFilled(new_tri, tris[i].colour);
+    drawStroke(new_tri, tris[i].colour);
   }
 }
 
@@ -431,7 +425,7 @@ void computeDepth(CanvasTriangle t, double *depthBuffer)
 }
 
 void depthBuffer(vector<ModelTriangle> tris)
-{ 
+{
   window.clearPixels();
   double *depthBuffer = (double*)malloc(sizeof(double) * WIDTH * HEIGHT);
   for(uint32_t y = 0; y < HEIGHT; y++)
@@ -451,76 +445,82 @@ void depthBuffer(vector<ModelTriangle> tris)
 
 void rotateX(float theta)
 {
-  mat3 rot = mat3(vec3(1, 0, 0), 
-                  vec3(0, cos(theta), -sin(theta)), 
-                  vec3(0, sin(theta), cos(theta)));
-  cameraOrientation *= rot;
+  mat3 rot(vec3(1.0, 0.0, 0.0),
+                  vec3(0.0, cos(theta), -sin(theta)),
+                  vec3(0.0, sin(theta), cos(theta)));
+  cameraOrientation = cameraOrientation * rot;
 }
 
 void rotateY(float theta)
 {
-  mat3 rot = mat3(vec3(cos(theta), 0, sin(theta)), 
-                  vec3(0, 1, 0), 
-                  vec3(-sin(theta), 0, cos(theta)));
-  cameraOrientation *= rot;
+  mat3 rot(vec3(cos(theta), 0.0, sin(theta)),
+                  vec3(0.0, 1.0, 0.0),
+                  vec3(-sin(theta), 0.0, cos(theta)));
+  cameraOrientation = cameraOrientation * rot;
+  // cout << glm::to_string(cameraOrientation) << '\n';
 }
 
 void rotateZ(float theta)
 {
-  mat3 rot = mat3(vec3(cos(theta), -sin(theta), 0), 
-                  vec3(sin(theta), cos(theta), 0), 
-                  vec3(0, 0, 1));
-  cameraOrientation *= rot;
+  mat3 rot(vec3(cos(theta), -sin(theta), 0.0),
+                  vec3(sin(theta), cos(theta), 0.0),
+                  vec3(0.0, 0.0, 1.0));
+  cameraOrientation = cameraOrientation * rot;
+}
+
+void lookAt()
+{
+
 }
 
 void handleEvent(SDL_Event event)
 {
   if(event.type == SDL_KEYDOWN) {
-    if(event.key.keysym.sym == SDLK_LEFT) 
+    if(event.key.keysym.sym == SDLK_LEFT)
     {
       cout << "TRANSLATE LEFT" << endl;
-      cameraPos.x += 10;
+      cameraPos.x -= 0.1;
     }
     else if(event.key.keysym.sym == SDLK_RIGHT)
     {
       cout << "TRANSLATE RIGHT" << endl;
-      cameraPos.x -= 10;
-    } 
+      cameraPos.x += 0.1;
+    }
     else if(event.key.keysym.sym == SDLK_UP)
     {
       cout << "TRANSLATE UP" << endl;
-      cameraPos.y -= 10;
-    } 
+      cameraPos.y -= 0.1;
+    }
     else if(event.key.keysym.sym == SDLK_DOWN)
     {
       cout << "TRANSLATE DOWN" << endl;
-      cameraPos.y += 10;
+      cameraPos.y += 0.1;
     }
     else if(event.key.keysym.sym == SDLK_c)
     {
       cout << "CLEAR SCREEN" << endl;
       window.clearPixels();
     }
-    else if(event.key.keysym.sym == SDLK_a)
-    {
-      cout << "ROTATE X" << endl;
-      rotateX(0.1);
-    } 
-    else if(event.key.keysym.sym == SDLK_d)
-    {
-      cout << "ROTATE X OTHER" << endl;
-      rotateX(-0.1);
-    } 
     else if(event.key.keysym.sym == SDLK_w)
     {
-      cout << "ROTATE Y" << endl;
-      rotateY(0.1);
-    } 
+      cout << "ROTATE X" << endl;
+      rotateX(0.01);
+    }
     else if(event.key.keysym.sym == SDLK_s)
     {
+      cout << "ROTATE X OTHER" << endl;
+      rotateX(-0.01);
+    }
+    else if(event.key.keysym.sym == SDLK_d)
+    {
+      cout << "ROTATE Y" << endl;
+      rotateY(0.01);
+    }
+    else if(event.key.keysym.sym == SDLK_a)
+    {
       cout << "ROTATE Y OTHER" << endl;
-      rotateY(-0.1);
-    } 
+      rotateY(-0.01);
+    }
 
   }
   else if(event.type == SDL_MOUSEBUTTONDOWN) cout << "MOUSE CLICKED" << endl;
